da74f9166c012d3822a6a4c98e81b5211c90ca89
Werner Donné <werner.donne@pincette.biz>
2021-09-11 16:08:23 +0200

    Update JSLT import and dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module net.pincette.json.streams.plugin {
   requires net.pincette.mongo.streams;
   requires net.pincette.mongo;
-  requires jslt;
+  requires com.schibsted.spt.data.jslt;
   exports net.pincette.json.streams.plugin;
 }
