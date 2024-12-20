c19c2c7e6e5232d9c40da23782a9dbfb6d42a090
Werner Donné <werner.donne@pincette.biz>
2024-12-20 23:55:34 +0100

    Update dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module net.pincette.json.streams.plugin {
   requires net.pincette.mongo.streams;
   requires net.pincette.mongo;
   requires com.schibsted.spt.data.jslt;
+
   exports net.pincette.json.streams.plugin;
 }
--- src/main/java/net/pincette/json/streams/plugin/Plugin.java
+++ src/main/java/net/pincette/json/streams/plugin/Plugin.java
@@ -10,18 +10,26 @@ import net.pincette.mongo.streams.Stage;
 /**
  * Implement this interface in a module if you want to add extensions to the JSON Streams runtime.
  *
- * @author Werner Donn\u00e9
+ * @author Werner Donné
  */
 public interface Plugin {
-  /** @return The MongoDB expression language operators. It may be <code>null</code>. */
+  /**
+   * @return The MongoDB expression language operators. It may be <code>null</code>.
+   */
   Map<String, Operator> expressionExtensions();
 
-  /** @return The JSLT custom functions. It may be <code>null</code>. */
+  /**
+   * @return The JSLT custom functions. It may be <code>null</code>.
+   */
   Collection<Function> jsltFunctions();
 
-  /** @return The MongoDB query language operators. It may be <code>null</code>. */
+  /**
+   * @return The MongoDB query language operators. It may be <code>null</code>.
+   */
   Map<String, QueryOperator> matchExtensions();
 
-  /** @return The MongoDB aggregation pipeline stages. It may be <code>null</code>. */
+  /**
+   * @return The MongoDB aggregation pipeline stages. It may be <code>null</code>.
+   */
   Map<String, Stage> stageExtensions();
 }
